30f63797be376c5f417bbf76b44beed38069b706
Krupamc <krupampatel710@gmail.com>
2026-07-03 16:31:51 -0400

    Message System almost working

--- config.py
+++ config.py
@@ -1,18 +1,54 @@
 # Config for the Status checker
 
 consecutive_offline = 3
+pytz_timezone = "US/Eastern"
 
-stations = [
-    "KNJBERKE13",
-    "KNJWARET3",
-    "KNJMANAH111",
-    "KNJMANAH7",
-    "KNJBAYHE18",
-    "KNJLONGB89",
-    "KNJLONGB21",
-    "KNJBEACH64",
-    "KNJOCEAN178",
-    "KNJSEASI44",
-    "KNJWARET3",
-]
-print(stations)
+#---Offline Emails---
+d_subject = "[OFFLINE] {station['name']} ({station_id}) appears offline"
+d_body = {
+    "Station {station_name} ({station_id}) appears OFFLINE on Weather Underground.\n"
+    "URL: {url}\n"
+    "Checked at: {now}\n"
+    "Consecutive offline checks: {consecutive_offline}\n"
+
+}
+
+# Email Setup
+server = "smtp.gmail.com"
+port = 587
+username = "theunknownboss1999@gmail.com"
+password = "dpdlzimvidrstktv"
+from_email = "Save Barnegat Bay Mesonet Notifications <theunknownboss1999@gmail.com>"
+
+
+# Email Addresses
+#global_recipients = ["krupampatel710@gmail.com", "outreach@savebarnegatbay.org"]
+global_recipients = ["krupampatel710@gmail.com"]
+
+recipients = {
+    #"KNJBERKE13": "",
+    #"KNJWARET3": "",
+    #"KNJMANAH111": "",
+    "KNJMANAH7": "fixmytechnj@gmail.com",
+    #"KNJBAYHE18": "",
+    #"KNJLONGB89": "",
+    #"KNJLONGB21": "",
+    #"KNJBEACH64": "",
+    #"KNJOCEAN178": "",
+    #"KNJSEASI44": "",
+    #"Ecocenter": ""
+}
+
+stations = {
+    "KNJBERKE13": "Island Beach State Park Bathhouse #1",
+    "KNJWARET3": "Sedge Island",
+    "KNJMANAH111": "Stafford Municipal Complex",
+    "KNJMANAH7": "MATES/Stafford",
+    "KNJBAYHE18": "Bayhead Fire Company #1",
+    "KNJLONGB89": "LBT Marine Field Station",
+    "KNJLONGB21": "LBI Foundation",
+    "KNJBEACH64": "Beachwood Yacht Club",
+    "KNJOCEAN178": "Ocean Gate Yacht Club",
+    "KNJSEASI44": "Seaside Park Yacht Club",
+    "Ecocenter": "Ecocenter",
+}
--- scrape.py
+++ scrape.py
@@ -2,39 +2,114 @@ import config as cfg
 import requests
 from bs4 import BeautifulSoup as bs
 from pathlib import Path
+import smtplib
+import ssl
+from email.message import EmailMessage
 import json
+from datetime import datetime, timezone
+import pytz
 
 station_id = "KNJMANAH7"
 
 def check_station(station_id):
-    url = f"https://preview.wunderground.com/dashboard/pws/{station_id}"
+    url = f"https://preview.wunderground.com/dashboard/pws/{station_id}" # Base url
 
     r = requests.get(url, timeout=10)
-    r.raise_for_status
+    r.raise_for_status()
 
     soup = bs(r.content, "html.parser")
 
+    # Get station name and status
     station_name = soup.find("h1").get_text()
     status_header = soup.find("pws-status")
 
     status = status_header['data-status']
 
-    print(f"Station: {station_name} [{status.upper()}]")
+    # time
+    utc_now = datetime.now(pytz.UTC)
+    eastern = pytz.timezone(cfg.pytz_timezone)
+    now = utc_now.astimezone(eastern)
 
     if status == "offline":
-        
+        # Increment offlines
+        data = read_json_file()
+        data[station_id]["consecutive_offline"] += 1
+        write_json_file(data)
+
+        data = read_json_file()
+        consec_offline = data[station_id]["consecutive_offline"]
+        if consec_offline >= cfg.consecutive_offline and not data[station_id]["alert_sent"]:
+            station_name = cfg.stations()
+            station_id = data[station_id]
+            offline_alert(station_id, station_name, consec_offline, now)
 
 def read_json_file():
-    with open("status.json", "r") as file:
-        json.load()
+    with open("status.json", "r", encoding="utf-8") as file:
+         data = json.load(file)
+    return data
 
 def write_json_file(data):
-    with open("status.json", "rw") as file:
-        json.dump(data, file, indent=4)
+    with open("status.json", "w", encoding="utf-8") as file:
+        json.dump(data, file, indent = 2)
 
-def write_start()
+def append_json_file(data):
+    with open("status.json", "a", encoding="utf-8") as file:
+        json.dump(data, file, indent=2)
+
+# Use all alert functions
+def offline_alert(station_id, station_name, consec_offline, now):
+
+    recipients = list(cfg.recipients[station_id].values()) + cfg.global_recipients
+
+    email(
+        subject = cfg.d_subject.format(station_name = station_name, station_id = station_id),
+        body = cfg.d_body.format(station_name = station_name, station_id = station_id, consecutive_offline = consec_offline, now=now),
+        recipients = recipients
+    )
+
+# Send an email
+def email(subject: str, body: str, recipients: list[str]) -> None:
+    
+    msg = EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = cfg.from_email
+    msg["To"] = ", ".join(recipients)
+    msg.set_content(body)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(cfg.server, cfg.port) as server:
+        server.starttls(context=context)
+        server.login(cfg.username, cfg.password)
+        server.send_message(msg)
+
+
+# Create base json status file
+def write_start():
+
     status_json_file = Path("status.json")
-    status_json_file.touch(exist_ok=True)
+    
+    if not status_json_file.exists():
+
+        data = {}
+
+        for station, station_name in cfg.stations:
+
+            data[station] = {
+                "station_id": station,
+                "station_name": station_name,
+                "consecutive_offline": 0,
+                "first_offline": None,
+                "last_status": "",
+                "alert_sent": False,
+                "since_sent": None,
+            }
+
+        write_json_file(data)
+        print("Json Status File Made")
+
+# Program
+
+write_start()
 
 for station in cfg.stations:
     check_station(station)
